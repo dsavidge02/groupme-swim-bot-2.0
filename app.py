e9f680b765657e0c82d9c5b6f498f8aff8a444fe
dsavidge02 <131463153+dsavidge02@users.noreply.github.com>
2023-05-08 15:51:06 -0400

    Update app.py
    added the anti spam and finished the event functionality - testing will occur

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import os
-import json
-import random
-
-import requests
-
 from datetime import datetime, timedelta
 import csv
 from csv import DictWriter
+import os
+import json
+import random
+import requests
 
 from flask import Flask, request
 app = Flask(__name__)
@@ -17,16 +15,20 @@ def webhook():
     print(data)
     checking = 0
     txt = data['text'].lower()
-    # We don't want to reply to ourselves!
-    if data['name'] == 'The Anti Palmer':
+    if data['name'] == 'MikeGPT':
         return "ok", 200
+    elif anti_spam():
+    	return "ok", 200
     elif 'palmer' in data['name'].lower() and checking==1:
         msg = 'I will break into your house and live inside your walls'
         send_message(msg, data['id'])
     elif is_question(txt) == 1:
-    	if question_handler(txt) == 1:
+    	questionNum = question_handler(txt)
+    	if questionNum == 1:
     		msg = shirt_question(txt)
     		send_message(msg, data['id'])
+    	elif questionNum == 2:
+    		event_question(txt)
     return "ok", 200
 
 @app.route('/', methods=['GET'])
@@ -48,7 +50,7 @@ def send_message(msg, reply_id):
          }
     print(info)
     requests.post(url, data=json.dumps(info))
-	
+
 def send_reminder(msg):
     url = 'https://api.groupme.com/v3/bots/post'
     info = {
@@ -57,13 +59,14 @@ def send_reminder(msg):
     }
     print(info)
     requests.post(url, data=json.dumps(info))
-	
+
 def read_lift_days():
 	file = open('liftdays.txt')
 	lines = file.readlines()
 	lift_days = [-1]*3
 	for i in range(0,3):
 		lift_days[i] = int(lines[i].strip())
+	file.close()
 	return lift_days
 
 def get_days():
@@ -78,6 +81,8 @@ def is_question(txt):
 def question_handler(txt):
 	if 'color' in txt or 'shirt' in txt: 
 		return 1
+	if 'events' in txt or 'event' in txt or 'meeting' in txt or 'meetings' in txt:
+		return 2
 	return 0
 
 def shirt_question(txt):
@@ -102,15 +107,33 @@ def whatShirt(a, d):
 	dow = days[day]
 	lift_days = read_lift_days()
 	if day == lift_days[0]:
-		return 'you should be wearing a black shirt!'
+		return 'You should be wearing a black shirt!'
 	elif day == lift_days[1]:
-		return 'you should be wearing a gray shirt!'
+		return 'You should be wearing a gray shirt!'
 	elif day == lift_days[2]:
-		return 'you should be wearing a red shirt!'
+		return 'You should be wearing a red shirt!'
 	else:
 		return 'I have not been taught what shirt you wear on ' + dow
 
+def event_question(txt):
+	eventFound = 0
+	if 'week' in txt:
+		eventFound = find_events(7)
+	else:
+		days = get_days()
+		days.append('today')
+		days.append('tomorrow')
+		for day in days:
+			if day in txt:
+				eventFound = find_daily_event(day)
+	if eventFound == 0:
+		msg = 'There doesn\'t seem to be any events'
+		send_reminder(msg)
+		#print(msg)
+
+#dayRange is how many days in the future would you like to send the reminder about
 def find_events(dayRange):
+	eventFound = 0
 	with open('team_events.csv') as csv_file:
 		csv_reader = csv.DictReader(csv_file)
 		for row in csv_reader:
@@ -128,9 +151,13 @@ def find_events(dayRange):
 					outfitString = ' and you should wear a ' + row["outfit"]
 				msg = 'We have a ' + row["event"] + dateString + ' in ' + row["location"] + outfitString
 				send_reminder(msg)
+				#print(msg)
+				eventFound = 1
 	csv_file.close()
+	return eventFound
 
 def find_daily_event(day_of_week):
+	eventFound = 0
 	days = get_days()
 	dow1 = day_of_week
 	if day_of_week == 'today':
@@ -145,8 +172,7 @@ def find_daily_event(day_of_week):
 		for row in csv_reader:
 			dt = datetime.strptime(row["dt"], '%Y-%m-%d %H:%M:%S')
 			dow = dt.strftime('%A').lower()
-			print(dow1 + ' ' + dow)
-			if dow == dow1 and dt <= datetime.now()+timedelta(hours=(24*7)):
+			if dow == dow1 and dt <= datetime.now()+timedelta(hours=(24*6)):
 				dateString1 = dt.strftime("%m/%d")
 				dateString = ' ('+dateString1+') at ' + dt.strftime("%I:%M %p")
 				outfitString = ''
@@ -154,6 +180,9 @@ def find_daily_event(day_of_week):
 					outfitString = ' and you should wear a ' + row["outfit"]
 				msg = 'We have a ' + row["event"] + ' ' + day_of_week + dateString + ' in ' + row["location"] + outfitString
 				send_reminder(msg)
+				#print(msg)
+				eventFound = 1
+	return eventFound
 
 def clear_events():
 	lines = list()
@@ -172,7 +201,148 @@ def clear_events():
 		writer = csv.writer(writeFile)
 		writer.writerow(field_names)
 		writer.writerows(lines) 
-				
+
+def get_event_people():
+	file = open('event_people.txt')
+	lines = file.readlines()
+	event_people = ['SOME RANDOM PERSON']*6
+	for i in range(0,6):
+		event_people[i] = lines[i].strip()
+	file.close()
+	return event_people
+
+def read_event(name, txt):
+	if name in get_event_people():
+		acceptable_events = ['meeting', 'event', 'activity']
+		event = 'NO EVENT'
+		for item in acceptable_events:
+			if item.lower() in txt.lower():
+				event  = item
+				break
+		acceptable_locations = ['Mueller', 'Harkness', 'Pool', 'Team Room', 'ECAV', 'Hall of Fame', 'TBD']
+		location = 'NO LOCATION'
+		for item in acceptable_locations:
+			if item.lower() in txt.lower():
+				location = item
+				break
+		acceptable_outfits = [' black ', 'gray ', ' grey ', ' red ', ' warmups ', ' quarterzip ', ' RPI Swim Gear ', ' RPI gear ', ' black.', 'gray.', ' grey.', ' red.', ' warmups.', ' quarterzip.', ' RPI Swim Gear.', ' RPI gear.']
+		outfit = ''
+		for item in acceptable_outfits:
+			if item.lower() in txt.lower():
+				outfit = item
+				break
+		dt = find_datetime(txt)
+		if event != 'NO EVENT' and location != 'NO LOCATION' and dt != -1:
+			create_event(dt, location, event, outfit)
+
+def find_datetime(txt):
+	date = find_date(txt)
+	adder = 0
+	if date == '':
+		temp = make_date(txt)
+		date = temp[0]
+		adder = temp[1]
+	temp2 = find_time(txt)
+	time = temp2[0]
+	adder2 = temp2[1]
+	if date != '' and time != '':
+		dt = datetime.strptime((date + ' ' + time), '%Y-%m-%d %H:%M:%S')
+		dt = dt + timedelta(days = adder) + timedelta(hours = adder2)
+		return dt
+	else:
+		return -1
+
+def make_date(txt):
+	date = datetime.now().strftime('%Y-%m-%d')
+	days = get_days()
+	day = -1
+	adder = 0
+	if 'today' in txt:
+		return [date,adder]
+	if 'tomorrow' in txt:
+		adder = 1
+		return [date,adder]
+	for i in range(0,len(days)):
+		if days[i] in txt:
+			possible_next = 'next ' + days[i]
+			if possible_next in txt:
+				adder = 7
+			day = i
+			curr = datetime.today().weekday()
+			if curr > day:
+				adder = adder + curr - day
+			else:
+				adder = adder + day - curr
+			return [date, adder]
+
+def find_date(txt):
+	i = txt.find('/')
+	date = ''
+	if i != -1:
+		i = txt.index('/')
+	else:
+		return date
+	while i != -1:
+		if txt[i-2].isnumeric():
+			date = date+txt[i-2]
+		else:
+			date = date + '0'
+		if txt[i-1].isnumeric():
+			date = date+txt[i-1]
+		date = date + '-'
+		if txt[i+1].isnumeric():
+			if not txt[i+2].isnumeric():
+				date = date + '0'
+			date = date+txt[i+1]
+			if txt[i+2].isnumeric():
+				date = date+txt[i+2]
+		if len(date) != 5:
+			date = ''
+			j = txt.find('/', i+1)
+			if j !=-1:
+				i = txt.index('/', i+1)
+			else:
+				return date
+		else:
+			i=-1
+	date = datetime.now().strftime('%Y') + '-' + date
+	return date
+
+def find_time(txt):
+	adder = 0
+	if 'pm' in txt.lower():
+		adder = 12
+	i = txt.find(':')
+	time = ''
+	if i != -1:
+		i = txt.index(':')
+	else:
+		return time
+	while i != -1:
+		if txt[i-2].isnumeric():
+			time = time+txt[i-2]
+		else:
+			time = time + '0'
+		if txt[i-1].isnumeric():
+			time = time+txt[i-1]
+		time = time + ':'
+		if txt[i+1].isnumeric():
+			time = time+txt[i+1]
+		if txt[i+2].isnumeric():
+			time = time+txt[i+2]
+		if len(time) != 5:
+			time = ''
+			j = txt.find(':', i+1)
+			if j != -1:
+				i = txt.index(':', i+1)
+			else:
+				return [time,adder]
+		else:
+			i=-1
+	time = time + ':00'
+	return [time,adder]
+
+
 def create_event(dt, location, event, outfit):
 	if(dt < datetime.now()):
 		return 0
@@ -189,11 +359,29 @@ def create_event(dt, location, event, outfit):
 		event_writer = DictWriter(team_events, fieldnames = field_names)
 		event_writer.writerow(dict)
 
+def anti_spam():
+	spam_detected = False
+	file = open('anti_spam.txt')
+	lines = file.readlines()
+	last_ping = datetime.strptime(lines[0].strip(), '%Y-%m-%d %H:%M:%S')
+	count_pings = int(lines[1].strip())+1
+	if count_pings >= 4 and datetime.now() - timedelta(seconds=30) < last_ping:
+		spam_detected = True
+	elif count_pings >= 4:
+		count_pings = 1
+	file.close()
+	with open('anti_spam.txt', 'w') as file:
+		file.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '\n' + str(count_pings) + '\n')
+	return spam_detected
+
 def main():
 	clear_events()
+	read_event('Andrew Palmer','There is a meeting next friday at 4:00pm in Mueller')
 	if datetime.today().weekday() == 6:
+		send_reminder('WEEKLY REMINDERS:')
 		find_events(7)
 	else:
+		send_reminder('DAILY REMINDERS:')
 		find_events(1)
 
 if __name__ == "__main__":
